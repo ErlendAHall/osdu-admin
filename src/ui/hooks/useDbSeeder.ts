d0ce03a45377f74a9534ec72b7779bb89cc77ecc
Erlend Hall <ehall@equinor.com>
2025-10-09 09:39:13 +0200

    added some more mock records

--- src/ui/hooks/useDbSeeder.ts
+++ src/ui/hooks/useDbSeeder.ts
@@ -5,8 +5,8 @@ import tubularAssemblySchema from "../../assets/mockSchemas/TubularAssembly:2.0.
 
 /* Mock records */
 import bhaRunRecord from "../../assets/mockRecords/BHARun:2.0.0:a828c845-101a-5ca0-a729-84fe19cf8841.json";
-import holeSectionRecord from "../../assets/mockRecords/HoleSectionRecord:1.4.0:9616795e-1fc1-50a2-b40f-e509668b514f.json";
-import tubularAssemblyRecord from "../../assets/mockRecords/TubularAssemblyRecord:2.0.0:6494d378-e4df-5ca2-a913-a061a085aada.json";
+import holeSectionRecord from "../../assets/mockRecords/HoleSection:1.4.0:9616795e-1fc1-50a2-b40f-e509668b514f.json";
+import tubularAssemblyRecord from "../../assets/mockRecords/TubularAssembly:2.0.0:6494d378-e4df-5ca2-a913-a061a085aada.json";
 
 import { useIndexedDb } from "./useIndexedDb.ts";
 import { useEffect } from "react";
