0bbd409854f03017fe4cbcb6da96cf42dab31bcc
Erlend Hall <ehall@equinor.com>
2025-10-14 08:07:25 +0200

    replaced colons in filenames for NTFS filesystems

--- src/ui/hooks/useDbSeeder.ts
+++ src/ui/hooks/useDbSeeder.ts
@@ -1,14 +1,14 @@
 /* Mock schemas */
-import bhaRunSchema from "../../assets/mockSchemas/BHARun:2.0.0.json";
-import holeSectionSchema from "../../assets/mockSchemas/HoleSection:1.4.0.json";
-import tubularAssemblySchema from "../../assets/mockSchemas/TubularAssembly:2.0.0.json";
+import bhaRunSchema from "../../assets/mockSchemas/BHARun--2.0.0.json";
+import holeSectionSchema from "../../assets/mockSchemas/HoleSection--1.4.0.json";
+import tubularAssemblySchema from "../../assets/mockSchemas/TubularAssembly--2.0.0.json";
 
 /* Mock records */
-import bhaRunRecord from "../../assets/mockRecords/BHARun:2.0.0:a828c845-101a-5ca0-a729-84fe19cf8841.json";
-import holeSectionRecord from "../../assets/mockRecords/HoleSection:1.4.0:9616795e-1fc1-50a2-b40f-e509668b514f.json";
-import tubularAssemblyRecord from "../../assets/mockRecords/TubularAssembly:2.0.0:6494d378-e4df-5ca2-a913-a061a085aada.json";
-import well1 from "../../assets/mockSchemas/Well:1.0.0.json";
-import well2 from "../../assets/mockSchemas/Well:1.4.0.json";
+import bhaRunRecord from "../../assets/mockRecords/BHARun--2.0.0--a828c845-101a-5ca0-a729-84fe19cf8841.json";
+import holeSectionRecord from "../../assets/mockRecords/HoleSection--1.4.0--9616795e-1fc1-50a2-b40f-e509668b514f.json";
+import tubularAssemblyRecord from "../../assets/mockRecords/TubularAssembly--2.0.0--6494d378-e4df-5ca2-a913-a061a085aada.json";
+import well1 from "../../assets/mockSchemas/Well--1.0.0.json";
+import well2 from "../../assets/mockSchemas/Well--1.4.0.json";
 
 import { useIndexedDb } from "./useIndexedDb.ts";
 import { useEffect, useState } from "react";
